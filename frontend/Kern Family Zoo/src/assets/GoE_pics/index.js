a2f6441ce121769e1adad45c63c80c4beac3306c
Alexander Kern <alexander.t.kern@gmail.com>
2023-01-14 14:52:27 -0800

    image constant

---
+++ frontend/Kern Family Zoo/src/assets/GoE_pics/index.js
@@ -0,0 +1,34 @@
+const IMAGES = {
+    frog1: require('./KS Frog x 14 11.jpg'),
+    bird1: require('./KS Moustache 14 11.jpg'), 
+    snake1: require('./GoE_pics/MDK_20070127_01772.jpg'),
+    snake2: require('./MDK_NGK_Cryptelytrops venustus_.jpg'),
+    chameleon1: require('./MDKern_20070210_00123from 2005 to 2006.jpg'),
+    spider1: require('./MDKern_Poecilotheria metallica - Saphaire Ornamental Tree Spider_0546.jpg'),
+    spider2: require('./MDKern_Poecilotheria rufilata, Slate Red Ornamental Tarantula_0560.jpg'),
+    spider3: require('./MDKern_Thelcticopis modesta  - Malaysian Orange Huntsman_0777.jpg'),
+    bird2: require('./Parc  30 24 Blue Angel jpg.jpg'),
+    snake3: require('./parc atheris squam blue eyes.jpg'),
+    snake4: require('./parc atheris squam red orange.jpg'),
+    bird3: require('./parc fa la la.jpg'),
+    bird4: require('./parc maile angel.jpg'),
+    lizard1: require('./parc monkey tail skink.jpg'),
+    bird5: require('./parc spock eating.jpg'),
+    bird6: require('./parc vcp 14 11.jpg'),
+    snake5: require('./parc_Boiga nigrocepes, Black Headed Cat Eye Snake_0136.jpg'),
+    lizard2: require('./parc_Iguana iguana, albino_0410.jpg'),
+    spider4: require('./parc_Megaphobema robustum, Columbian Giant Redleg Tarantula_0457-Edit.jpg'),
+    spider5: require('./parc_Poecilotheria metallica - Saphaire Ornamental Tree Spider_0547.jpg'),
+    frog2: requires('./parc_Theloderma corticale, Vietnamese Mossy Frog_0782.jpg'),
+    frog3: requires('./parc_UKR_Anotheca spinosa, Spiny headed Treefrog_33.jpg'),
+    gecko1: requires('./parc_Uroplatus fimbriatus_0877.jpg'),
+    gecko2: requries('./parc_Uroplatus sikorae - Mossy leaf-tailed gecko_0921.jpg'),
+    snake5: requires('./parc_Usamba Bush Viper (Atheris cerataphora)_40.jpg'),
+
+
+    
+
+};
+
+export default IMAGES;
+
