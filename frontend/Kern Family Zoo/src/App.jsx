ab0ee693ab6a46b87a6c3e7c293e14f5739ff268
Alexander Kern <alexander.t.kern@gmail.com>
2023-01-27 07:44:11 -0800

    import react router dom

--- frontend/Kern Family Zoo/src/App.jsx
+++ frontend/Kern Family Zoo/src/App.jsx
@@ -1,4 +1,5 @@
 import { useState, useEffect} from 'react'
+import {Route, Switch} from "react-router-dom"
 import './App.css'
 import './styles/gallery.css'
 import { GalleryColumn } from './components/galleryColumn';
