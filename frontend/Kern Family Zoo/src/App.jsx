5a3ce6c23274603038cf057f0299a491654a83d9
Alexander Kern <alexander.t.kern@gmail.com>
2023-02-04 16:53:10 -0800

    added import for router

--- frontend/Kern Family Zoo/src/App.jsx
+++ frontend/Kern Family Zoo/src/App.jsx
@@ -1,4 +1,5 @@
 import { useState, useEffect} from 'react'
+import {BrowserRouter, Route, Link } from "react-router-dom"
 import './App.css'
 import './styles/gallery.css'
 import { GalleryColumn } from './components/galleryColumn';
