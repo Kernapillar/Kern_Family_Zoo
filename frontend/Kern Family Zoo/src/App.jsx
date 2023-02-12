e6f22fdd8078a17104e7adc31637cfd4bfddf8c8
Alexander Kern <alexander.t.kern@gmail.com>
2023-02-12 10:20:21 -0800

    setup router

--- frontend/Kern Family Zoo/src/App.jsx
+++ frontend/Kern Family Zoo/src/App.jsx
@@ -1,5 +1,5 @@
 import { useState, useEffect} from 'react'
-import {BrowserRouter, Route, Link } from "react-router-dom"
+import {Routes, Route, Link, BrowserRouter } from "react-router-dom"
 import './App.css'
 import './styles/gallery.css'
 import { GalleryColumn } from './components/galleryColumn';
@@ -65,105 +65,15 @@ function App() {
   
   ]
 
-//   const [columns, setColumns] = useState(5);
-//   const [width, setWidth] = useState(window.innerWidth);
-//   const [images, setImages] = useState(pics);
-
-//   useEffect(() => {
-//     if (width < 700) {
-//       setColumns(1);
-//     } else if (width < 1200) {
-//       setColumns(2);
-//     } else if (width < 1500) {
-//       setColumns(3);
-//     } else if (width < 1700) {
-//       setColumns(4);
-//     } else {
-//       setColumns(5)
-//     }
-//     const handleResize = () => setWidth(window.innerWidth);
-//     window.addEventListener("resize", handleResize);
-//     return () => {
-//       window.removeEventListener("resize", handleResize);
-//     };
-
-//   });
-  
-//   const updateFilter = (num) => {
-//       if (num === -1) {
-//         setImages(pics);
-//       } else {
-//         setImages(pics.filter(img => img.legs === num))
-//       }
-//   };
-
-//   const renderNav = () => {
-//     return (
-//       <nav className='navbar'>
-//         <button className='filter-button' onClick={() => updateFilter(-1)}>clear filter</button>
-//         <button className='filter-button' onClick={() => updateFilter(0)}>0 legs</button>
-//         <button className='filter-button' onClick={() => updateFilter(2)}>2 legs</button>
-//         <button className='filter-button' onClick={() => updateFilter(4)}>4 legs</button>
-//         <button className='filter-button' onClick={() => updateFilter(8)}>8 legs</button>
-//       </nav>
-//     )
-//   };
-
-//   const colArr = [[],[], [], [], []];
-
-//   const renderCols = () => {
-//     if (columns === 1) {
-//       return (
-//         <div className='gallery'>
-//         <GalleryColumn pics={colArr[0]}/>
-//       </div>
-//       )
-//     } else if (columns === 2) {
-//       return (
-//         <div className='gallery'>
-//         <GalleryColumn pics={colArr[0]}/>
-//         <GalleryColumn pics={colArr[1]}/>
-//       </div>
-//       )
-//     } else if (columns === 3) {
-//       return (
-//         <div className='gallery'>
-//         <GalleryColumn pics={colArr[0]}/>
-//         <GalleryColumn pics={colArr[1]}/>
-//         <GalleryColumn pics={colArr[2]}/>
-//       </div>
-//       )
-//     }  else if (columns === 4) {
-//       return (
-//         <div className='gallery'>
-//         <GalleryColumn pics={colArr[0]}/>
-//         <GalleryColumn pics={colArr[1]}/>
-//         <GalleryColumn pics={colArr[2]}/>
-//         <GalleryColumn pics={colArr[3]}/>
-//       </div>
-//       )
-//     } else {
-//       return (
-//         <div className='gallery'>
-//         <GalleryColumn pics={colArr[0]}/>
-//         <GalleryColumn pics={colArr[1]}/>
-//         <GalleryColumn pics={colArr[2]}/>
-//         <GalleryColumn pics={colArr[3]}/>
-//         <GalleryColumn pics={colArr[4]}/>
-//       </div>
-//       )
-//     }
-//   };
-//   for (let i = 0; i < images.length; i++) {
-//     colArr[i % columns].push(images[i])
-//   }
 
   return (
-    <>
+    <BrowserRouter>
       <img src={LOGO} alt="" className='logo' />
-      <Gallery pics={pics}/>
-    </>
+      <Routes>
+        <Route path="/" element={<Gallery pics={pics}/>}/>
+      </Routes>
     
+    </BrowserRouter>
   )
 }
 
